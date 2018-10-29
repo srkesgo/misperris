ecd8abc0ff056086efa8039ce99d8ee01654383f
moises <mo.sepulvedar@profesor.duoc.cl>
2018-10-29 17:31:59 -0300

    se agrega login con facebook

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,11 +2,25 @@ from django import forms
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.forms import ValidationError
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(required=True, label="Nombre")
     last_name = forms.CharField(required=True, label="Apellido")
+
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        usuario = User.objects.filter(email = email)
+
+        if usuario:
+            raise ValidationError("El email ya existe")
+        
+        return email
+    
+        
+
 
     class Meta:
         #le diremos al formulario que debe guardar los datos
--- automotora/settings.py
+++ automotora/settings.py
@@ -39,6 +39,10 @@ LOGOUT_REDIRECT_URL = "/accounts/login"
 
 # Application definition
 
+SOCIAL_AUTH_FACEBOOK_KEY = '2221703071486402'  # App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '58ad44a7981b98d58ff67a54e040f1f4'  # App Secret
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,7 +52,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'accounts',
+    'social_django',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',#se agrega esta linea
 ]
 
 ROOT_URLCONF = 'automotora.urls'
@@ -73,6 +82,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',  # se agrega esta linea
+                'social_django.context_processors.login_redirect', # se agrega esta linea
             ],
         },
     },
@@ -129,3 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
