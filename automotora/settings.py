40fea9478f476874f158a0860e4eb989b2f048ba
SDP13005 <SDP13005>
2018-10-30 19:06:57 -0300

    se quita crispy

--- automotora/settings.py
+++ automotora/settings.py
@@ -53,10 +53,8 @@ INSTALLED_APPS = [
     'core',
     'accounts',
     'social_django',
-    'crispy_forms',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
